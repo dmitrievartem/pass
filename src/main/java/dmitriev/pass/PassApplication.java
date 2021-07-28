0dca25c3a656196b406bf92d41a8b64af51b17ea
Артем Дмитриев <artem2733@yandex.ru>
2021-07-28 19:28:01 +0300

    init

---
+++ src/main/java/dmitriev/pass/PassApplication.java
@@ -0,0 +1,13 @@
+package dmitriev.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitriev/pass/PassApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitriev.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
