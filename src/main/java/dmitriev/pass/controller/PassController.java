e3be4aba7d90e86e38316fc7143c1aa3c11d8ec6
Артем Дмитриев <artem2733@yandex.ru>
2021-07-28 20:56:28 +0300

    add http methods to PassController

--- src/main/java/dmitriev/pass/controller/PassController.java
+++ src/main/java/dmitriev/pass/controller/PassController.java
@@ -1,8 +1,7 @@
 package dmitriev.pass.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dmitriev.pass.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,7 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("pass")
 public class PassController {
-    public List<Map<String, String>> passes = new ArrayList<Map<String, String>>() {{
+    private int counter = 4;
+    private List<Map<String, String>> passes = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String, String>() {{ put("id", "1"); put("text", "first text"); }});
         add(new HashMap<String, String>() {{ put("id", "2"); put("text", "second text"); }});
         add(new HashMap<String, String>() {{ put("id", "3"); put("text", "third text"); }});
@@ -21,5 +21,41 @@ public class PassController {
     @GetMapping
     public List<Map<String, String>> list() {
         return passes;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getPass(id);
+    }
+
+    private Map<String, String> getPass(String id) {
+        return passes.stream()
+                .filter(pass -> pass.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> pass) {
+        pass.put("id", String.valueOf(counter++));
+        passes.add(pass);
+        return pass;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> pass) {
+        Map<String, String> passFromDb = getPass(id);
+
+        passFromDb.putAll(pass);
+        passFromDb.put("id", id);
+
+        return passFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void update(@PathVariable String id) {
+        Map<String, String> pass = getPass(id);
+
+        passes.remove(pass);
     }
 }
