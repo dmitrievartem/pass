451b9d696a545a58c74f18f771852b311c7f39e2
Артем Дмитриев <artem2733@yandex.ru>
2021-07-28 22:51:01 +0300

    add database connection and change PassController

--- src/main/java/dmitriev/pass/controller/PassController.java
+++ src/main/java/dmitriev/pass/controller/PassController.java
@@ -1,61 +1,50 @@
 package dmitriev.pass.controller;
 
-import dmitriev.pass.exceptions.NotFoundException;
+import dmitriev.pass.domain.Pass;
+import dmitriev.pass.repo.PassRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("pass")
 public class PassController {
-    private int counter = 4;
-    private List<Map<String, String>> passes = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "first text"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "second text"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "third text"); }});
-    }};
+    private final PassRepo passRepo;
+
+    @Autowired
+    public PassController(PassRepo passRepo) {
+        this.passRepo = passRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return passes;
+    public List<Pass> list() {
+        return passRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) {
-        return getPass(id);
-    }
-
-    private Map<String, String> getPass(String id) {
-        return passes.stream()
-                .filter(pass -> pass.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
-
-    @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> pass) {
-        pass.put("id", String.valueOf(counter++));
-        passes.add(pass);
+    public Pass getOne(@PathVariable("id") Pass pass) {
         return pass;
     }
 
+    @PostMapping
+    public Pass create(@RequestBody Pass pass) {
+        return passRepo.save(pass);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> pass) {
-        Map<String, String> passFromDb = getPass(id);
+    public Pass update(
+            @PathVariable("id") Pass passFromDb,
+            @RequestBody Pass pass
+    ) {
+        BeanUtils.copyProperties(pass, passFromDb,  "id");
 
-        passFromDb.putAll(pass);
-        passFromDb.put("id", id);
-
-        return passFromDb;
+        return passRepo.save(passFromDb);
     }
 
     @DeleteMapping("{id}")
-    public void update(@PathVariable String id) {
-        Map<String, String> pass = getPass(id);
-
-        passes.remove(pass);
+    public void update(@PathVariable("id") Pass pass) {
+        passRepo.delete(pass);
     }
 }
